dd53cc58e5d9fe0f7e930378c8c9bd02534d6029
thanga-v2 <thangarajm@icloud.com>
2025-04-15 11:34:17 +0530

    client

--- mcp-client/client.py
+++ mcp-client/client.py
@@ -3,4 +3,24 @@ from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
+async def main():
+    server_script_path = "/Users/thangaraj/Documents/projects/MCP/MCP_fahd/mcp-hello-server/hello.py"
+
+    server_params = StdioServerParameters(
+        command="python",
+        args=[server_script_path],
+        env=None
+    )
+
+    async with AsyncExitStack() as stack:
+        stdio_transport = await stack.enter_async_context(stdio_client(server_params))
+        session = await stack.enter_async_context(ClientSession(*stdio_transport))
+
+        await session.initialize()
+        result = await session.call_tool("say_hello", {"name" : "Thanga MCP"})
+
+        print(result.content.text)
+
+asyncio.run(main())        
+
 
